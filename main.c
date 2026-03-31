f48225ef2c989a922238784eb2c69863ce8db948
muhammadanggersatrio@gmail.com <muhammadanggersatrio@gmail.com>
2026-03-31 21:06:00 +0700

    Update konversiSuhu

--- main.c
+++ main.c
@@ -1,41 +1,92 @@
-#include stdio.h
+#include <stdio.h>
 
-void konversiSuhu();
-void konversiJarak();
-void konversiBerat();
-void konversiWaktu();
+void konversiSuhu(){
+    int kon, versi;
+    float suhu, hasil;
+
+    printf("Masukan suhu =");
+    scanf("%f", &suhu);
+
+    printf("Konversi \n1. Celcius\n2. Fahreinheit\n3. Kelvin\n= ");
+    scanf("%d", &kon);
+
+    switch (kon) {
+    case 1:
+        printf("Menjadi \n1. Fahreinheit\n2. Kelvin\n= ");
+        scanf("%d", &versi);
+        if (versi == 1)
+            hasil = (suhu * 9/5) + 32;
+        else if (versi == 2)
+            hasil = suhu + 273;
+        else
+            hasil = suhu;
+        break;
+    case 2:
+        printf("Menjadi \n1. Celcius\n2. Kelvin\n= ");
+        scanf("%d", &versi);
+        if (versi == 1)
+            hasil = (suhu - 32) * 5 / 9;
+        else if (versi == 2)
+            hasil = (suhu - 32) * 5 / 9 + 273;
+        else
+            hasil = suhu;
+        break;
+    case 3:
+        printf("Menjadi \n1. Celcius\n2. Fahreinheit\n= ");
+        scanf("%d", &versi);
+        if (versi == 1)
+            hasil = suhu - 273;
+        else if (versi == 2)
+            hasil = (suhu - 273) * 9 / 5 + 32;
+        else
+            hasil = suhu;
+        break;
+    default:
+        break;
+    }
+
+    if (hasil == suhu)
+        printf("Tolong input ulang");
+    else
+        printf("%.2f", hasil);
+}
+
+void konversiJarak(){}
+void konversiBerat(){}
+void konversiWaktu(){}
 
 int main(){
-    int pilihan;
+    int pilihan, jalan;
 
-    while(){
-        Printf("\n === KALKULATOR KONVERSI SATUAN ===");
-        Printf(" 1. konversi suhu\n");
-        Printf(" 2. konversi jarak\n");
-        Printf(" 3. konversi Berat\n");
-        Printf(" 4. konversi waktu\n");
+    while(jalan){
+        printf("\n === KALKULATOR KONVERSI SATUAN ===\n");
+        printf(" 1. konversi suhu\n2. konversi jarak\n3. konversi Berat\n4. konversi waktu\n5. selesai\n");
 
         scanf("%d", &pilihan);
 
         switch(pilihan) {
             case 1:
                 konversiSuhu();
+                jalan = 0;
                 break;
             case 2: 
-                konversijarak();
+                konversiJarak();
+                jalan = 0;
                 break;
             case 3:
                 konversiBerat();
+                jalan = 0;
                 break;
             case 4:
                 konversiWaktu();
+                jalan = 0;
                 break;
             case 5:
                 printf("Terimakasih!\n");
-                exit(0);
+                jalan = 0;
+                break;
             default:
-                printf("Pilihan tidak valid!\n")
+                printf("Pilihan tidak valid!\n");
         }
     }
-    return 0;
 }
